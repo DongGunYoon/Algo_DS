224fbfd96fde4714383911009358eb8e8703c355
DongGunYoon <donggun961108@gmail.com>
2021-03-28 00:50:24 +0900

    3월 28일 5일차 과제

---
+++ mar_26th/README.md
@@ -0,0 +1,5 @@
+# 3월 28일 5일차 과제
+
+### Node를 사용해 BinaryTree 구현하기
+
+- [1. Source Code](./nodeBinaryTree.py)
--- mar_26th/nodeBinaryTree.py
+++ mar_26th/nodeBinaryTree.py
@@ -19,16 +19,101 @@ class BinaryTree:
         self.root = node_list[0]
 
     def preorder(self):
-        pass
-    
+        result = '['
+        def recursive(node):
+            nonlocal result
+            if node is None:
+                return
+            
+            result += str(node.value) + ' '
+
+            recursive(node.left)
+            recursive(node.right)
+        
+        recursive(self.root)
+        print(result.rstrip() + ']')
+
     def inorder(self):
-        pass
+        result = '['
+        def recursive(node):
+            nonlocal result
+            if node is None:
+                return
+
+            recursive(node.left)
+            result += str(node.value) + ' '
+            recursive(node.right)
+        
+        recursive(self.root)
+        print(result.rstrip() + ']')
     
     def postorder(self):
-        pass
+        result = '['
+        def recursive(node):
+            nonlocal result
+            if node is None:
+                return
+
+            recursive(node.left)
+            recursive(node.right)
+            result += str(node.value) + ' '
+        
+        recursive(self.root)
+        print(result.rstrip() + ']')
 
     def bfs(self, value):
-        return False
-    
+        isFound = False
+        queue = [self.root]
+        while queue:
+            pop = queue.pop(0)
+            if pop.value == value:
+                isFound = True
+                return isFound
+            else:
+                if pop.left is not None:
+                    queue.append(pop.left)
+                if pop.right is not None:
+                    queue.append(pop.right)
+        
+        return isFound
+
     def dfs(self, value):
-        return False
+        isFound = False
+
+        def recursive(node):
+            nonlocal isFound
+
+            if node is None:
+                return
+            
+            if node.value == value:
+                isFound = True
+                return isFound
+
+            recursive(node.left)
+            recursive(node.right)
+        
+        recursive(self.root)
+        return isFound
+
+# 1 ~ 10까지의 수를 포함한 노드 리스트 생성
+nodes = [i for i in range(1, 11)]
+# 이진트리에 해당 노드들 삽입
+tree = BinaryTree(nodes)
+
+# 깊이 우선 순회 테스트
+tree.preorder()
+
+# 대칭 순회 테스트
+tree.inorder()
+
+# 후위 순회 테스트
+tree.postorder()
+
+# dfs 정상 작동 테스트
+print(tree.dfs(1))
+print(tree.dfs(15))
+
+# bfs 정상 작동 테스트
+print(tree.bfs(5))
+print(tree.bfs(12))
