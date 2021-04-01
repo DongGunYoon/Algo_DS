aac621f346ff3b712b81948dd7970a7400d392fc
DongGunYoon <donggun961108@gmail.com>
2021-04-01 20:00:56 +0900

    3월 31일 과제 완료

--- mar_31st/quickSort.py
+++ mar_31st/quickSort.py
@@ -1,29 +1,35 @@
 # 4월 1일 수정 예정
 
-# def quicksort(x):
-#     def recursive(start, end):
-#         if start >= end:
-#             return
+def quick_sort(x):
+    def recursive(start, end):
+        if start >= end:
+            return
 
-#         left, right = start + 1, end
-#         pivot = x[start]
+        left, right = start + 1, end
+        pivot = x[start]
 
-#         while left <= right:
-#             while left <= right:
-#                 if x[left] > pivot:
-#                     break
-#                 left += 1
-#             while left <= right:
-#                 if x[right] < pivot:
-#                     break
-#                 right -= 1
+        while left <= right:
+            while left <= right:
+                if x[right] < pivot:
+                    break
+                right -= 1 
+            while left <= right:
+                if x[left] > pivot:
+                    break
+                left += 1
+
+            if right >= left:
+                x[left], x[right] = x[right], x[left]
             
-#             if left <= right:
-#                 x[left], x[right] = x[right], x[left]
+        x[right], x[start] = x[start], x[right]
 
-#         x[left], x[start] = x[start], x[left]        
-#         recursive(start, left - 1)
-#         recursive(left+1, end)
+        recursive(start, right - 1)
+        recursive(right+1, end)
 
-#     recursive(0, len(x) - 1)
-#     print(x)
+    recursive(0, len(x) - 1)
+    print(x)
+
+s = [4, 7, 9, 6, 3, 5]
+a = [5, 24 , 5, 7, 8, 9, 4, 8, 12, 7, 32, 7, 2]
+
+quick_sort(a)
