75ab8184e44808014c0477238d0fa8046436ccb2
DongGunYoon <donggun961108@gmail.com>
2021-04-08 00:54:53 +0900

    April 7th HW

--- april_7th/solution.py
+++ april_7th/solution.py
@@ -3,20 +3,16 @@ def solution(words, queries):
     
     for i in range(len(queries)):
         count = queries[i].count('?')
-        length = len(queries[i])
         ind = queries[i].index('?')
-        sum = 0
 
         for word in words:
-            if len(word) != length:
+            if len(word) != len(queries[i]):
                 continue
             elif ind == 0:
                 if word[count:] == queries[i][count:]:
-                    sum += 1
+                    result[i] += 1
             else:
                 if word[:ind] == queries[i][:ind]:
-                    sum += 1
-    
-            result[i] = sum
+                    result[i] += 1
 
     return result
